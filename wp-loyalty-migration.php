12c925ce3c684ea9eb7a0d78dbedb4922156be20
srikumar03 <srikumarkgm369@gmail.com>
2024-12-10 13:35:09 +0530

    Refactor: Improve code style and WooCommerce compatibility
    Improve code style and add compatibility check for WooCommerce features using `Automattic\WooCommerce\Utilities\FeaturesUtil`.  This ensures better compatibility and maintainability.

--- wp-loyalty-migration.php
+++ wp-loyalty-migration.php
@@ -19,68 +19,80 @@
  * WPLoyalty Page Link: wp-loyalty-migration
  */
 
-defined( 'ABSPATH' ) or die();
-if ( ! function_exists( 'isWLRMGWooCommerceActive' ) ) {
-	function isWLRMGWooCommerceActive() {
-		$active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins', [] ) );
-		if ( is_multisite() ) {
-			$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', [] ) );
-		}
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
-		return in_array( 'woocommerce/woocommerce.php', $active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', $active_plugins );
-	}
+defined('ABSPATH') or die();
+
+
+add_action('before_woocommerce_init', function () {
+    if (class_exists(FeaturesUtil::class)) {
+        FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__);
+    }
+});
+
+if (!function_exists('isWLRMGWooCommerceActive')) {
+    function isWLRMGWooCommerceActive()
+    {
+        $active_plugins = apply_filters('active_plugins', get_option('active_plugins', []));
+        if (is_multisite()) {
+            $active_plugins = array_merge($active_plugins, get_site_option('active_sitewide_plugins', []));
+        }
+
+        return in_array('woocommerce/woocommerce.php', $active_plugins) || array_key_exists('woocommerce/woocommerce.php', $active_plugins);
+    }
 }
 
-if ( ! function_exists( 'isWLRMGLoyaltyActive' ) ) {
-	function isWLRMGLoyaltyActive() {
-		$active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins', [] ) );
-		if ( is_multisite() ) {
-			$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', [] ) );
-		}
+if (!function_exists('isWLRMGLoyaltyActive')) {
+    function isWLRMGLoyaltyActive()
+    {
+        $active_plugins = apply_filters('active_plugins', get_option('active_plugins', []));
+        if (is_multisite()) {
+            $active_plugins = array_merge($active_plugins, get_site_option('active_sitewide_plugins', []));
+        }
 
-		return in_array( 'wp-loyalty-rules/wp-loyalty-rules.php', $active_plugins ) || array_key_exists( 'wp-loyalty-rules/wp-loyalty-rules.php', $active_plugins );
-	}
+        return in_array('wp-loyalty-rules/wp-loyalty-rules.php', $active_plugins) || array_key_exists('wp-loyalty-rules/wp-loyalty-rules.php', $active_plugins);
+    }
 }
 
-if ( ! isWLRMGWooCommerceActive() || ! isWLRMGLoyaltyActive() ) {
-	return;
+if (!isWLRMGWooCommerceActive() || !isWLRMGLoyaltyActive()) {
+    return;
 }
 
-defined( 'WLRMG_PLUGIN_NAME' ) or define( 'WLRMG_PLUGIN_NAME', 'WPLoyalty - Migration' );
-defined( 'WLRMG_PLUGIN_VERSION' ) or define( 'WLRMG_PLUGIN_VERSION', '1.0.0' );
-defined( 'WLRMG_TEXT_DOMAIN' ) or define( 'WLRMG_TEXT_DOMAIN', 'wp-loyalty-migration' );
-defined( 'WLRMG_PLUGIN_SLUG' ) or define( 'WLRMG_PLUGIN_SLUG', 'wp-loyalty-migration' );
-defined( 'WLRMG_PLUGIN_PATH' ) or define( 'WLRMG_PLUGIN_PATH', str_replace( "\\", '/', __DIR__ ) . '/' );
-defined( 'WLRMG_PLUGIN_DIR' ) or define( 'WLRMG_PLUGIN_DIR', str_replace( "\\", '/', __DIR__ ) );
-defined( 'WLRMG_PLUGIN_URL' ) or define( 'WLRMG_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-defined( 'WLRMG_PLUGIN_FILE' ) or define( 'WLRMG_PLUGIN_FILE', __FILE__ );
-defined( 'WLRMG_PLUGIN_AUTHOR' ) or define( 'WLRMG_PLUGIN_AUTHOR', 'WPLoyalty' );
-defined( 'WLRMG_VIEW_PATH' ) or define( 'WLRMG_VIEW_PATH', str_replace( "\\", '/', __DIR__ ) . '/App/Views' );
-defined( 'WLRMG_MINIMUM_PHP_VERSION' ) or define( 'WLRMG_MINIMUM_PHP_VERSION', '7.0.0' );
-defined( 'WLRMG_MINIMUM_WP_VERSION' ) or define( 'WLRMG_MINIMUM_WP_VERSION', '4.9' );
-defined( 'WLRMG_MINIMUM_WC_VERSION' ) or define( 'WLRMG_MINIMUM_WC_VERSION', '6.0' );
-defined( 'WLRMG_MINIMUM_WLR_VERSION' ) or define( 'WLRMG_MINIMUM_WLR_VERSION', '1.2.10' );
+defined('WLRMG_PLUGIN_NAME') or define('WLRMG_PLUGIN_NAME', 'WPLoyalty - Migration');
+defined('WLRMG_PLUGIN_VERSION') or define('WLRMG_PLUGIN_VERSION', '1.0.0');
+defined('WLRMG_TEXT_DOMAIN') or define('WLRMG_TEXT_DOMAIN', 'wp-loyalty-migration');
+defined('WLRMG_PLUGIN_SLUG') or define('WLRMG_PLUGIN_SLUG', 'wp-loyalty-migration');
+defined('WLRMG_PLUGIN_PATH') or define('WLRMG_PLUGIN_PATH', str_replace("\\", '/', __DIR__) . '/');
+defined('WLRMG_PLUGIN_DIR') or define('WLRMG_PLUGIN_DIR', str_replace("\\", '/', __DIR__));
+defined('WLRMG_PLUGIN_URL') or define('WLRMG_PLUGIN_URL', plugin_dir_url(__FILE__));
+defined('WLRMG_PLUGIN_FILE') or define('WLRMG_PLUGIN_FILE', __FILE__);
+defined('WLRMG_PLUGIN_AUTHOR') or define('WLRMG_PLUGIN_AUTHOR', 'WPLoyalty');
+defined('WLRMG_VIEW_PATH') or define('WLRMG_VIEW_PATH', str_replace("\\", '/', __DIR__) . '/App/Views');
+defined('WLRMG_MINIMUM_PHP_VERSION') or define('WLRMG_MINIMUM_PHP_VERSION', '7.0.0');
+defined('WLRMG_MINIMUM_WP_VERSION') or define('WLRMG_MINIMUM_WP_VERSION', '4.9');
+defined('WLRMG_MINIMUM_WC_VERSION') or define('WLRMG_MINIMUM_WC_VERSION', '6.0');
+defined('WLRMG_MINIMUM_WLR_VERSION') or define('WLRMG_MINIMUM_WLR_VERSION', '1.2.10');
 
-if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	return;
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+    return;
 }
 require_once __DIR__ . '/vendor/autoload.php';
 
 $myUpdateChecker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
-	'https://github.com/wployalty/wployalty_migration',
-	__FILE__,
-	'wp-loyalty-migration'
+    'https://github.com/wployalty/wployalty_migration',
+    __FILE__,
+    'wp-loyalty-migration'
 );
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
-add_filter( 'plugins_loaded', function () {
-	if ( ! class_exists( '\Wlr\App\Helpers\Input' ) ) {
-		return;
-	}
-	if ( ! class_exists( '\Wlrm\App\Router' ) ) {
-		return;
-	}
-	\Wlrm\App\Setup::init(); // init setup
-	if ( \Wlrm\App\Helper\Plugin::checkDependencies() ) {
-		\Wlrm\App\Router::init();
-	}
-} );
+add_filter('plugins_loaded', function () {
+    if (!class_exists('\Wlr\App\Helpers\Input')) {
+        return;
+    }
+    if (!class_exists('\Wlrm\App\Router')) {
+        return;
+    }
+    \Wlrm\App\Setup::init(); // init setup
+    if (\Wlrm\App\Helper\Plugin::checkDependencies()) {
+        \Wlrm\App\Router::init();
+    }
+});
