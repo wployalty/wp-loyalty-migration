66fe584035deb29378edf8214390bce19c721e53
srikumar03 <srikumarkgm369@gmail.com>
2024-12-20 16:26:11 +0530

    Refactor: Improve Pagination Helper
    Improve the pagination link generation logic for betterclarity, efficiency, and maintainability.  The URL
    construction is simplified to use `http_build_query` for
    handling query parameters more effectively.

--- App/Helper/Pagination.php
+++ App/Helper/Pagination.php
@@ -79,108 +79,88 @@ class Pagination
      * @return string
      * @since 1.0.0
      */
+    /**
+     * Generate the pagination links
+     *
+     * @param array $pagination_args
+     *
+     * @return string
+     * @since 1.0.0
+     */
     function createLinks($pagination_args = array())
     {
-        // If total number of rows is zero, do not need to continue
-        if ($this->totalRows == 0 or $this->perPage == 0) {
+        if ($this->totalRows == 0 || $this->perPage == 0) {
             return '';
         }
 
-        // Calculate the total number of pages
         $numPages = ceil($this->totalRows / $this->perPage);
-
-        // If there is only one page, return the count information
         if ($numPages == 1) {
             if ($this->showCount) {
                 $info = 'Showing : ' . $this->totalRows;
                 return ' <div class="dataTables_info">' . $info . '</div>';
-            } else {
-                return '';
             }
+            return '';
         }
 
-        // Here, we adjust the query string to add `type=woocommerce_migration`
-        $query_string_sep_data = (isset($pagination_args['page_number_name']) && !empty($pagination_args['page_number_name'])) ? $pagination_args['page_number_name'] . '=' : 'migration_page=';
-
-        // Determine the base URL and add additional parameters
-        $query_string_sep = '';
         $input = new Input();
-
-        $get_type = 'woocommerce_migration';  // Always set to 'woocommerce_migration' as per your requirement
-
-        // Construct the query string with the additional parameters
-        if (isset($get_job_id) && !empty($get_job_id)) {
-            $query_string_sep .= '?job_id=' . $get_job_id;
-        }
-        $query_string_sep .= '&type=' . $get_type; // Always include `type=woocommerce_migration`
-
-        // Ensure that the `migration_page` is correctly added to the URL
-        if (empty($query_string_sep)) {
-            $query_string_sep .= (strpos($this->baseURL, '?') === false) ? '?' . $query_string_sep_data : '&amp;' . $query_string_sep_data;
-        } else {
-            $query_string_sep .= '&amp;' . $query_string_sep_data;
-        }
-
-        $this->baseURL = $this->baseURL . $query_string_sep;
-        // Determine the current page
         $this->currentPage = $input->post_get($this->queryStringSegment, 0);
         if (!is_numeric($this->currentPage) || $this->currentPage == 0) {
             $this->currentPage = 1;
         }
-        // Links content string variable
+
+        $baseParams = $_GET; // Retrieve current query parameters
+        unset($baseParams[$this->queryStringSegment]); // Remove page_number to dynamically append
+
+        $queryString = http_build_query($baseParams); // Build the query string
+        $baseURLWithParams = $this->baseURL . ($queryString ? '?' . $queryString . '&' : '?') . $this->queryStringSegment . '=';
+
         $output = '';
         $this->numLinks = (int)$this->numLinks;
-        // Is the page number beyond the result range? the last page will show
         if ($this->currentPage > $this->totalRows) {
             $this->currentPage = $numPages;
         }
         $uriPageNum = $this->currentPage;
-        // Calculate the start and end numbers.
         $start = (($this->currentPage - $this->numLinks) > 0) ? $this->currentPage - ($this->numLinks - 1) : 1;
         $end = (($this->currentPage + $this->numLinks) < $numPages) ? $this->currentPage + $this->numLinks : $numPages;
 
         $focusId = (isset($pagination_args['focus_id']) && !empty($pagination_args['focus_id'])) ? "#" . $pagination_args['focus_id'] : $this->focusId;
 
-        // Render the "First" link
         if ($this->currentPage > $this->numLinks) {
-            //$query_string_sep .= 1;
-            //$firstPageURL = str_replace($query_string_sep, '', $this->baseURL);
-            $output .= $this->firstTagOpen . '<a href="' . $this->baseURL . '1' . $focusId . '">' . $this->firstLink . '</a>' . $this->firstTagClose;
+            $output .= $this->firstTagOpen . '<a href="' . $baseURLWithParams . '1' . $focusId . '">' . $this->firstLink . '</a>' . $this->firstTagClose;
         }
-        // Render the "previous" link
+
         if ($this->currentPage != 1) {
             $i = ($uriPageNum - 1);
             if ($i == 0) {
                 $i = '';
             }
-            $output .= $this->prevTagOpen . '<a href="' . $this->baseURL . $i . $focusId . '">' . $this->prevLink . '</a>' . $this->prevTagClose;
+            $output .= $this->prevTagOpen . '<a href="' . $baseURLWithParams . $i . $focusId . '">' . $this->prevLink . '</a>' . $this->prevTagClose;
         }
-        // Write the digit links
+
         for ($loop = $start - 1; $loop <= $end; $loop++) {
             $i = $loop;
             if ($i >= 1) {
                 if ($this->currentPage == $loop) {
                     $output .= $this->curTagOpen . $loop . $this->curTagClose;
                 } else {
-                    $output .= $this->numTagOpen . '<a href="' . $this->baseURL . $i . $focusId . '">' . $loop . '</a>' . $this->numTagClose;
+                    $output .= $this->numTagOpen . '<a href="' . $baseURLWithParams . $i . $focusId . '">' . $loop . '</a>' . $this->numTagClose;
                 }
             }
         }
-        // Render the "next" link
+
         if ($this->currentPage < $numPages) {
             $i = ($this->currentPage + 1);
-            $output .= $this->nextTagOpen . '<a href="' . $this->baseURL . $i . $focusId . '">' . $this->nextLink . '</a>' . $this->nextTagClose;
+            $output .= $this->nextTagOpen . '<a href="' . $baseURLWithParams . $i . $focusId . '">' . $this->nextLink . '</a>' . $this->nextTagClose;
         }
-        // Render the "Last" link
+
         if (($this->currentPage + $this->numLinks) < $numPages) {
             $i = $numPages;
-            $output .= $this->lastTagOpen . '<a href="' . $this->baseURL . $i . $focusId . '">' . $this->lastLink . '</a>' . $this->lastTagClose;
+            $output .= $this->lastTagOpen . '<a href="' . $baseURLWithParams . $i . $focusId . '">' . $this->lastLink . '</a>' . $this->lastTagClose;
         }
-        // Remove double slashes
+
         $output = preg_replace("#([^:])//+#", "\\1/", $output);
-        // Add the wrapper HTML if exists
         $output = $this->fullTagOpen . $output . $this->fullTagClose;
-        // Showing links notification
+
         if ($this->showCount) {
             $currentOffset = ($this->currentPage > 1) ? ($this->currentPage - 1) * $this->perPage : $this->currentPage;
             $info = 'Showing ' . $currentOffset . ' to ';
@@ -196,4 +176,5 @@ class Pagination
 
         return $output;
     }
+
 }
