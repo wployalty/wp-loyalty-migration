0ce862c7740c8e2b19ae86c9ef3d9600877dac8d
sabhari-krr <sabhari.a.krr@gmail.com>
2025-08-18 12:16:53 +0530

    Update minimum WooCommerce version requirement to 10.0.0 for compatibility.

--- wp-loyalty-migration.php
+++ wp-loyalty-migration.php
@@ -9,7 +9,7 @@
  * Text Domain: wp-loyalty-migration
  * Domain Path: /i18n/languages/
  * Requires at least: 4.9.0
- * WC requires at least: 6.5
+ * WC requires at least: 10.0.0
  * WC tested up to: 9.4
  * Contributors: WPLoyalty
  * Author URI: https://wployalty.net/
@@ -74,7 +74,7 @@ defined( 'WLRMG_PLUGIN_AUTHOR' ) or define( 'WLRMG_PLUGIN_AUTHOR', 'WPLoyalty' )
 defined( 'WLRMG_VIEW_PATH' ) or define( 'WLRMG_VIEW_PATH', str_replace( "\\", '/', __DIR__ ) . '/App/Views' );
 defined( 'WLRMG_MINIMUM_PHP_VERSION' ) or define( 'WLRMG_MINIMUM_PHP_VERSION', '7.0.0' );
 defined( 'WLRMG_MINIMUM_WP_VERSION' ) or define( 'WLRMG_MINIMUM_WP_VERSION', '4.9' );
-defined( 'WLRMG_MINIMUM_WC_VERSION' ) or define( 'WLRMG_MINIMUM_WC_VERSION', '6.5' );
+defined( 'WLRMG_MINIMUM_WC_VERSION' ) or define( 'WLRMG_MINIMUM_WC_VERSION', '10.0.0' );
 defined( 'WLRMG_MINIMUM_WLR_VERSION' ) or define( 'WLRMG_MINIMUM_WLR_VERSION', '1.2.10' );
 
 if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
