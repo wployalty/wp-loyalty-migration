006a4a8624af52005fcb0dd0c88c6bb90fb51997
sabhari-krr <sabhari.a.krr@gmail.com>
2025-09-05 10:03:55 +0530

    refactor: format warning message.

--- App/Controller/Common.php
+++ App/Controller/Common.php
@@ -460,12 +460,14 @@ class Common {
 			'cancel_export_warning' => __( 'Export is in progress. Do you really want to close?', 'wp-loyalty-migration' ),
 			'cancel_warning' => __( 'Make sure you want to close?', 'wp-loyalty-migration' ),
 			'migration_notice' => wp_kses_post( '
-                    <h3>' . __( 'Important Note : Please read before starting migration,Do not deactivate or delete your old point system/program during migration.' ,'wp-loyalty-migration') . '</h3>
-                    <ul>
+                    <h3>' . __( 'Important Note : ' ,'wp-loyalty-migration') . '</h3>
+					<p>' . __('Please read before starting migration:' ,'wp-loyalty-migration') . '</p>
+                    <ol>
+						<li>' . __('Do not deactivate or delete your old point system/program during migration.' ,'wp-loyalty-migration') . '</li>
                         <li>' . __( 'Before starting the migration in WPLoyalty, ensure that earning and redeeming points is paused in your existing system/program. Its lead to give extra/low points in migration.' ,'wp-loyalty-migration') . '</li>
                         <li>' . __( 'The default batch limit is set to 50. You can adjust this in the settings.For example, if the batch limit is 50, then 150 customers will be migrated every 3 minutes — resulting in 3000 customers per hour, around 72,000 customers per day.','wp-loyalty-migration' ) . '</li>
                         <li>' . __( 'Once a migration job is started, it cannot be paused or stopped midway. Please double-check all configurations before initiating the process.' ,'wp-loyalty-migration') . '</li>
-                    </ul>
+                    </ol>
                 ' ),
 		],true );
 		wp_localize_script( WLRMG_PLUGIN_SLUG . '-main-script', 'wlrmg_localize_data', $localize_data );
