d035f99f7e654323b9a5c8108b6ac45d5c497491
sabhari-krr <sabhari.a.krr@gmail.com>
2025-09-12 12:22:43 +0530

    code format changes

--- App/Router.php
+++ App/Router.php
@@ -12,36 +12,34 @@ use Wlrmg\App\Controller\Common;
 use Wlrmg\App\Controller\Migration;
 use Wlrmg\App\Controller\MigrationRunner;
 
-defined('ABSPATH') or die();
+defined( 'ABSPATH' ) or die();
 
-class Router
-{
+class Router {
 
-    /**
-     * @return void
-     */
-    static function init()
-    {
-        if (is_admin()) {
-            add_action('admin_menu', [Common::class, 'addMenu'], 11);
-            add_action('admin_footer', [Common::class, 'hideMenu']);
-            add_action('admin_enqueue_scripts', [Common::class, 'addAssets']);
+	/**
+	 * @return void
+	 */
+	static function init() {
+		if ( is_admin() ) {
+			add_action( 'admin_menu', [ Common::class, 'addMenu' ], 11 );
+			add_action( 'admin_footer', [ Common::class, 'hideMenu' ] );
+			add_action( 'admin_enqueue_scripts', [ Common::class, 'addAssets' ] );
 
-	        if (wp_doing_ajax()) {
-                add_action('wp_ajax_wlrmg_confirm_update_points', [Migration::class, 'getConfirmContent']);
-                add_action('wp_ajax_wlrmg_migrate_users', [Migration::class, 'migrateUsersWithPointsJob']);
-                add_action('wp_ajax_wlrmg_save_settings', [Migration::class, 'saveSettings']);
-                add_action('wp_ajax_wlrmg_export_popup', [Migration::class, 'exportPopup']);
-                add_action('wp_ajax_wlrmg_handle_export', [Migration::class, 'handleExport']);
-                add_action('wp_ajax_wlrmg_get_exported_list', [Migration::class, 'getExportFiles']);
-            }
-        }
-        add_filter('wlr_extra_action_list', [Common::class, 'addExtraAction'], 10, 1);
-        add_filter('wlr_extra_point_ledger_action_list', [Common::class, 'addExtraPointLedgerAction'], 10, 1);
+			if ( wp_doing_ajax() ) {
+				add_action( 'wp_ajax_wlrmg_confirm_update_points', [ Migration::class, 'getConfirmContent' ] );
+				add_action( 'wp_ajax_wlrmg_migrate_users', [ Migration::class, 'migrateUsersWithPointsJob' ] );
+				add_action( 'wp_ajax_wlrmg_save_settings', [ Migration::class, 'saveSettings' ] );
+				add_action( 'wp_ajax_wlrmg_export_popup', [ Migration::class, 'exportPopup' ] );
+				add_action( 'wp_ajax_wlrmg_handle_export', [ Migration::class, 'handleExport' ] );
+				add_action( 'wp_ajax_wlrmg_get_exported_list', [ Migration::class, 'getExportFiles' ] );
+			}
+		}
+		add_filter( 'wlr_extra_action_list', [ Common::class, 'addExtraAction' ], 10, 1 );
+		add_filter( 'wlr_extra_point_ledger_action_list', [ Common::class, 'addExtraPointLedgerAction' ], 10, 1 );
 
-        add_action('woocommerce_init', [Common::class, 'initSchedule'], 10);
-        add_filter('cron_schedules', [Common::class, 'addMinutes']);
-        add_action('wlrmg_migration_jobs', [Common::class, 'triggerMigrations']);
-        add_action('wlrmg_process_migration_job', [MigrationRunner::class, 'processAction']);
-    }
+		add_action( 'woocommerce_init', [ Common::class, 'initSchedule' ], 10 );
+		add_filter( 'cron_schedules', [ Common::class, 'addMinutes' ] );
+		add_action( 'wlrmg_migration_jobs', [ Common::class, 'triggerMigrations' ] );
+		add_action( 'wlrmg_process_migration_job', [ MigrationRunner::class, 'processAction' ] );
+	}
 }
--- App/Setup.php
+++ App/Setup.php
@@ -6,95 +6,87 @@ use Wlrmg\App\Helper\Plugin;
 use Wlrmg\App\Models\MigrationLog;
 use Wlrmg\App\Models\ScheduledJobs;
 
-defined('ABSPATH') || exit();
+defined( 'ABSPATH' ) || exit();
 
-class Setup
-{
-    public static function init()
-    {
-        register_activation_hook(WLRMG_PLUGIN_FILE, [__CLASS__, 'activate']);
-        register_deactivation_hook(WLRMG_PLUGIN_FILE, [__CLASS__, 'deactivate']);
-        register_uninstall_hook(WLRMG_PLUGIN_FILE, [__CLASS__, 'uninstall']);
+class Setup {
+	public static function init() {
+		register_activation_hook( WLRMG_PLUGIN_FILE, [ __CLASS__, 'activate' ] );
+		register_deactivation_hook( WLRMG_PLUGIN_FILE, [ __CLASS__, 'deactivate' ] );
+		register_uninstall_hook( WLRMG_PLUGIN_FILE, [ __CLASS__, 'uninstall' ] );
 
-        add_filter('plugin_row_meta', [__CLASS__, 'getPluginRowMeta'], 10, 2);
-        add_action('plugins_loaded', [__CLASS__, 'maybeRunMigration']);
-        add_action('upgrader_process_complete', [__CLASS__, 'maybeRunMigration']);
-    }
+		add_filter( 'plugin_row_meta', [ __CLASS__, 'getPluginRowMeta' ], 10, 2 );
+		add_action( 'plugins_loaded', [ __CLASS__, 'maybeRunMigration' ] );
+		add_action( 'upgrader_process_complete', [ __CLASS__, 'maybeRunMigration' ] );
+	}
 
-    /**
-     * Run plugin activation scripts.
-     */
-    public static function activate()
-    {
-        Plugin::checkDependencies(true);
-        self::maybeRunMigration();
-    }
+	/**
+	 * Run plugin activation scripts.
+	 */
+	public static function activate() {
+		Plugin::checkDependencies( true );
+		self::maybeRunMigration();
+	}
 
-    /**
-     * Maybe run database migration.
-     */
-    public static function maybeRunMigration()
-    {
-        $db_version = get_option('wlrm_version', '0.0.1');
-        if (version_compare($db_version, WLRMG_PLUGIN_VERSION, '<')) {
-            self::runMigration();
-            update_option('wlrm_version', WLRMG_PLUGIN_VERSION);
-        }
-    }
+	/**
+	 * Maybe run database migration.
+	 */
+	public static function maybeRunMigration() {
+		$db_version = get_option( 'wlrm_version', '0.0.1' );
+		if ( version_compare( $db_version, WLRMG_PLUGIN_VERSION, '<' ) ) {
+			self::runMigration();
+			update_option( 'wlrm_version', WLRMG_PLUGIN_VERSION );
+		}
+	}
 
-    /**
-     * Run database migration.
-     */
-    private static function runMigration()
-    {
-        $models = [
-            new ScheduledJobs(),
-            new MigrationLog()
-        ];
-        foreach ($models as $model) {
-            if (is_a($model, '\Wlr\App\Models\Base')) {
-                $model->create();
-            }
-        }
-    }
+	/**
+	 * Run database migration.
+	 */
+	private static function runMigration() {
+		$models = [
+			new ScheduledJobs(),
+			new MigrationLog()
+		];
+		foreach ( $models as $model ) {
+			if ( is_a( $model, '\Wlr\App\Models\Base' ) ) {
+				$model->create();
+			}
+		}
+	}
 
-    /**
-     * Run plugin activation scripts.
-     */
-    public static function deactivate()
-    {
-        $next_scheduled = wp_next_scheduled("wlrmg_migration_jobs");
-        wp_unschedule_event($next_scheduled, "wlrmg_migration_jobs");
-        if (function_exists('as_unschedule_all_actions')) {
-            as_unschedule_all_actions('', [], 'wlrmg_migration_queue');
-        }
-    }
+	/**
+	 * Run plugin activation scripts.
+	 */
+	public static function deactivate() {
+		$next_scheduled = wp_next_scheduled( "wlrmg_migration_jobs" );
+		wp_unschedule_event( $next_scheduled, "wlrmg_migration_jobs" );
+		if ( function_exists( 'as_unschedule_all_actions' ) ) {
+			as_unschedule_all_actions( '', [], 'wlrmg_migration_queue' );
+		}
+	}
 
-    /**
-     * Run plugin activation scripts.
-     */
-    public static function uninstall()
-    {
-        // silence is golden
-    }
+	/**
+	 * Run plugin activation scripts.
+	 */
+	public static function uninstall() {
+		// silence is golden
+	}
 
-    /**
-     * Retrieves the plugin row meta to be displayed on the Woocommerce appointments plugin page.
-     *
-     * @param array $links The existing plugin row meta links.
-     * @param string $file The path to the plugin file.
-     *
-     * @return array
-     */
-    public static function getPluginRowMeta($links, $file)
-    {
-        if ($file != plugin_basename(WLRMG_PLUGIN_FILE)) {
-            return $links;
-        }
-        $row_meta = [
-            'support' => '<a href="' . esc_url('https://wployalty.net/support/') . '" aria-label="' . esc_attr__('Support', 'wp-loyalty-migration') . '">' . esc_html__('Support', 'wp-loyalty-migration') . '</a>',
-        ];
+	/**
+	 * Retrieves the plugin row meta to be displayed on the Woocommerce appointments plugin page.
+	 *
+	 * @param array $links The existing plugin row meta links.
+	 * @param string $file The path to the plugin file.
+	 *
+	 * @return array
+	 */
+	public static function getPluginRowMeta( $links, $file ) {
+		if ( $file != plugin_basename( WLRMG_PLUGIN_FILE ) ) {
+			return $links;
+		}
+		$row_meta = [
+			'support' => '<a href="' . esc_url( 'https://wployalty.net/support/' ) . '" aria-label="' . esc_attr__( 'Support', 'wp-loyalty-migration' ) . '">' . esc_html__( 'Support', 'wp-loyalty-migration' ) . '</a>',
+		];
 
-        return array_merge($links, $row_meta);
-    }
+		return array_merge( $links, $row_meta );
+	}
 }
